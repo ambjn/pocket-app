1963d91fc621d707889624bff7cb0bfe1cd5c2db
Amber Jain <jainanuamber@gmail.com>
2025-12-11 01:03:18 +0530

    feat: Implement initial login UI with `KeyboardAvoidingView` and `KeyboardProvider`.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
-import { Stack } from 'expo-router'
+import { Stack } from 'expo-router';
+import { KeyboardProvider } from "react-native-keyboard-controller";
 
 const RootLayout = () => {
     return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }} />
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-
-        </Stack>
+        <KeyboardProvider>
+            <Stack>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            </Stack>
+        </KeyboardProvider>
     )
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { COLORS } from '@/utils/Colors'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView } from 'react-native-keyboard-controller'
 
 const Page = () => {
     // return <Redirect href={'/(tabs)/(saves)'} />
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Page</Text>
-        </View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.logo}>
+                    <Image source={require('@/assets/images/pocket-logo.png')} style={styles.logoIcon} />
+                </View>
+                <Text style={styles.title}>Log In</Text>
+            </View>
+        </KeyboardAvoidingView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white,
+        paddingHorizontal: 30,
+        paddingTop: 80,
+    },
+    header: {
+        alignItems: 'center',
+        marginBottom: 40
+    },
+    logo: {
+        width: 80,
+        height: 80,
+        marginBottom: 30
+    },
+    logoIcon: {
+        width: 80,
+        height: 80,
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: COLORS.textDark,
+    },
+})
 
 export default Page
