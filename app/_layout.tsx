d06040bc9fba9d6857675b30a518745572511a00
Amber Jain <jainanuamber@gmail.com>
2025-12-10 23:35:38 +0530

    feat: Implement tab-based navigation with Home and Settings screens, add color utilities, and update app identifiers and build commands.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,17 @@
-import { Stack } from "expo-router";
+import { COLORS } from '@/utils/Colors';
+import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <NativeTabs tintColor={COLORS.textDark}>
+      <NativeTabs.Trigger name="home">
+        <Label>Home</Label>
+        <Icon sf={{ default: 'house', selected: 'house.fill' }} drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="settings">
+        <Icon sf={{ default: 'gearshape', selected: 'gearshape.fill' }} drawable="custom_settings_drawable" />
+        <Label>Settings</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
