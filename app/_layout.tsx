d5e1962febf6e54517cd41307f5c76d2f548725e
Amber Jain <jainanuamber@gmail.com>
2025-12-10 23:51:40 +0530

    fix routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,15 +4,15 @@ import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 export default function RootLayout() {
   return (
     <NativeTabs tintColor={COLORS.textDark}>
-      <NativeTabs.Trigger name="home">
+      <NativeTabs.Trigger name="(home)">
         <Label>Home</Label>
         <Icon sf={{ default: 'house', selected: 'house.fill' }} drawable="custom_android_drawable" />
       </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="saves">
+      <NativeTabs.Trigger name="(saves)">
         <Icon sf={{ default: 'heart', selected: 'heart.fill' }} drawable="custom_settings_drawable" />
         <Label>Saves</Label>
       </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="settings">
+      <NativeTabs.Trigger name="(settings)">
         <Icon sf={{ default: 'gearshape', selected: 'gearshape.fill' }} drawable="custom_settings_drawable" />
         <Label>Settings</Label>
       </NativeTabs.Trigger>
